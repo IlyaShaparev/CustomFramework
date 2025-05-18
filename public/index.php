05b5f4bd9e8458b1b285cda55775e6538d476c05
Ilya <bis12012003@yandex.ru>
2025-05-18 21:30:34 +0300

    feat(routing): install packet for formatted print errors

--- public/index.php
+++ public/index.php
@@ -1,10 +1,14 @@
 <?php
-
-require_once dirname(__DIR__).'/vendor/autoload.php';
+define('BASE_PATH', dirname(__DIR__));
+require_once BASE_PATH.'/vendor/autoload.php';
 
 use DeParis\Kernel\Http\Core;
 use DeParis\Kernel\Http\Request;
 
+// TODO: Убрать по завершению работ
+$whoops = new Whoops\Run;
+$whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
+$whoops->register();
 $request = Request::createFromGlobals();
 
 $core = new Core();
