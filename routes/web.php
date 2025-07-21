22206695cd7b4a241166480ad4ed2b89f46160bc
D E _ P A R I S <95442603+IlyaShaparev@users.noreply.github.com>
2025-07-22 00:47:31 +0300

    feat(fixes): fix PR (#5)

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,7 @@ use DeParis\Kernel\Routing\Route;
 return [
     Route::get('/', [HomeController::class, 'index']),
     Route::get('/cars/{name}', [CarController::class, 'show']),
+    Route::get('/city/{name}', function (string $name) {
+        return new \DeParis\Kernel\Http\Response("Wow! We are in $name");
+    })
 ];
